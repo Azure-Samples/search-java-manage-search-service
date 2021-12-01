a071ea17b1ff38ec427be49e4a72ef68d9998225
Weidong Xu <weidxu@microsoft.com>
2021-12-01 14:35:54 +0800

    Release 2.10.0

--- src/main/java/com/azure/resourcemanager/search/samples/ManageSearchService.java
+++ src/main/java/com/azure/resourcemanager/search/samples/ManageSearchService.java
@@ -1,26 +1,25 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.search.samples;
+package com.azure.resourcemanager.search.samples;
 
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.azure.management.search.AdminKeyKind;
-import com.microsoft.azure.management.search.SearchService;
-import com.microsoft.azure.management.search.SkuName;
-import com.microsoft.rest.LogLevel;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.samples.Utils;
+import com.azure.resourcemanager.search.models.AdminKeyKind;
+import com.azure.resourcemanager.search.models.SearchService;
+import com.azure.resourcemanager.search.models.SkuName;
 
-import java.io.File;
 import java.util.Date;
-import java.util.List;
 
 /**
- * Azure Search sample for managing search service.
+ * Azure Cognitive Search sample for managing search service.
  *  - Create a Search service resource with a free SKU
  *  - Create a Search service resource with a standard SKU, one replica and one partition
  *  - Create a new query key and delete a query key
@@ -32,20 +31,20 @@ public class ManageSearchService {
     /**
      * Main function which runs the actual sample.
      *
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure) {
-        final String rgName = SdkContext.randomResourceName("rgSearch", 15);
-        final String searchServiceName = SdkContext.randomResourceName("ssrv", 20);
+    public static boolean runSample(AzureResourceManager azureResourceManager) {
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgSearch", 15);
+        final String searchServiceName = Utils.randomResourceName(azureResourceManager, "ssrv", 20);
         final Region region = Region.US_EAST;
 
         try {
-            
+
             //=============================================================
             // Check if the name for the Azure Search service to be created is available
 
-            if (!azure.searchServices().checkNameAvailability(searchServiceName).isAvailable()) {
+            if (!azureResourceManager.searchServices().checkNameAvailability(searchServiceName).isNameAvailable()) {
                 return false;
             }
             Date t1, t2;
@@ -53,7 +52,7 @@ public class ManageSearchService {
             // Azure limits the number of free Search service resource to one per subscription
             // List all Search services in the subscription and skip if there is already one resource of type free SKU
             boolean createFreeService = true;
-            List<SearchService> resources = azure.searchServices().list();
+            PagedIterable<SearchService> resources = azureResourceManager.searchServices().list();
             for (SearchService item : resources) {
                 if (item.sku().name() == SkuName.FREE) {
                     createFreeService = false;
@@ -69,7 +68,7 @@ public class ManageSearchService {
 
                 t1 = new Date();
 
-                SearchService searchServiceFree = azure.searchServices().define(searchServiceName + "free")
+                SearchService searchServiceFree = azureResourceManager.searchServices().define(searchServiceName + "free")
                     .withRegion(region)
                     .withNewResourceGroup(rgName)
                     .withFreeSku()
@@ -87,13 +86,13 @@ public class ManageSearchService {
 
             t1 = new Date();
 
-            SearchService searchService = azure.searchServices().define(searchServiceName)
-                    .withRegion(region)
-                    .withNewResourceGroup(rgName)
-                    .withStandardSku()
-                    .withPartitionCount(1)
-                    .withReplicaCount(1)
-                    .create();
+            SearchService searchService = azureResourceManager.searchServices().define(searchServiceName)
+                .withRegion(region)
+                .withNewResourceGroup(rgName)
+                .withStandardSku()
+                .withPartitionCount(1)
+                .withReplicaCount(1)
+                .create();
 
             t2 = new Date();
             System.out.println("Created Azure Search service: (took " + ((t2.getTime() - t1.getTime()) / 1000) + " seconds) " + searchService.id());
@@ -105,8 +104,8 @@ public class ManageSearchService {
 
             System.out.println("List all the Azure Search services for a given resource group");
 
-            for (SearchService service : azure.searchServices().listByResourceGroup(rgName)) {
-              Utils.print(service);
+            for (SearchService service : azureResourceManager.searchServices().listByResourceGroup(rgName)) {
+                Utils.print(service);
             }
 
 
@@ -133,12 +132,12 @@ public class ManageSearchService {
             System.out.println("Update an Azure Search service");
 
             searchService = searchService.update()
-                    .withTag("tag2", "value2")
-                    .withTag("tag3", "value3")
-                    .withoutTag("tag1")
-                    .withReplicaCount(2)
-                    .withPartitionCount(2)
-                    .apply();
+                .withTag("tag2", "value2")
+                .withTag("tag3", "value3")
+                .withoutTag("tag1")
+                .withReplicaCount(2)
+                .withPartitionCount(2)
+                .apply();
 
             Utils.print(searchService);
 
@@ -148,7 +147,7 @@ public class ManageSearchService {
 
             System.out.println("Delete a query key for an Azure Search service");
 
-            searchService.deleteQueryKey(searchService.listQueryKeys().get(1).key());
+            searchService.deleteQueryKey(searchService.listQueryKeys().iterator().next().key());
 
             Utils.print(searchService);
 
@@ -158,7 +157,7 @@ public class ManageSearchService {
 
             System.out.println("Delete an Azure Search service resource");
 
-            azure.searchServices().deleteByResourceGroup(rgName, searchServiceName);
+            azureResourceManager.searchServices().deleteByResourceGroup(rgName, searchServiceName);
 
             return true;
         } catch (Exception f) {
@@ -167,7 +166,7 @@ public class ManageSearchService {
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().beginDeleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
@@ -180,7 +179,6 @@ public class ManageSearchService {
 
     /**
      * Main entry point.
-     *
      * @param args the parameters
      */
     public static void main(String[] args) {
@@ -188,17 +186,20 @@ public class ManageSearchService {
             //=============================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .build();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.BODY)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure);
+            runSample(azureResourceManager);
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
